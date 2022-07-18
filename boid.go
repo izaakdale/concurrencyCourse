509710432add40cf691fd8870f7df236ebe92c29
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-07-18 18:09:22 +0100

    Copied boid from repo

--- boid.go
+++ boid.go
@@ -12,56 +12,10 @@ type Boid struct {
 	id       int
 }
 
-func createBoid(id int) {
-	b := Boid{
-		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
-		velocity: Vector2D{(rand.Float64() * 2) - 1.0, (rand.Float64() * 2) - 1.0},
-		id:       id,
-	}
-	boids[id] = &b
-	boidMap[int(b.position.x)][int(b.position.y)] = b.id
-	go b.start()
-}
-
-func (b *Boid) start() {
-	for {
-		b.moveOne()
-		time.Sleep(5 * time.Millisecond)
-	}
-}
-
-func (b *Boid) moveOne() {
-	accel := b.calcAccel()
-	rWlock.RLock()
-	b.velocity = b.velocity.Add(accel).limit(-1, 1)
-	boidMap[int(b.position.x)][int(b.position.y)] = -1
-	b.position = b.position.Add(b.velocity)
-	boidMap[int(b.position.x)][int(b.position.y)] = b.id
-	next := b.position.Add(b.velocity)
-	if next.x >= screenWidth || next.x < 0 {
-		b.velocity = Vector2D{-b.velocity.x, b.velocity.y}
-	}
-	if next.y >= screenHeight || next.y < 0 {
-		b.velocity = Vector2D{b.velocity.x, -b.velocity.y}
-	}
-	rWlock.RUnlock()
-}
-
-func (b *Boid) borderBounce(pos, maxBorderPos float64) float64 {
-	if pos < viewRadius {
-		return 1 / pos
-	} else if pos > maxBorderPos-viewRadius {
-		return 1 / (pos - maxBorderPos)
-	}
-	return 0
-}
-
-func (b *Boid) calcAccel() Vector2D {
-
+func (b *Boid) calcAcceleration() Vector2D {
 	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
 	avgPosition, avgVelocity, separation := Vector2D{0, 0}, Vector2D{0, 0}, Vector2D{0, 0}
 	count := 0.0
-
 	rWlock.RLock()
 	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i++ {
 		for j := math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j++ {
@@ -76,6 +30,7 @@ func (b *Boid) calcAccel() Vector2D {
 		}
 	}
 	rWlock.RUnlock()
+
 	accel := Vector2D{b.borderBounce(b.position.x, screenWidth), b.borderBounce(b.position.y, screenHeight)}
 	if count > 0 {
 		avgPosition, avgVelocity = avgPosition.DivisionV(count), avgVelocity.DivisionV(count)
@@ -85,5 +40,41 @@ func (b *Boid) calcAccel() Vector2D {
 		accel = accel.Add(accelAlignment).Add(accelCohesion).Add(accelSeparation)
 	}
 	return accel
+}
 
+func (b *Boid) borderBounce(pos, maxBorderPos float64) float64 {
+	if pos < viewRadius {
+		return 1 / pos
+	} else if pos > maxBorderPos-viewRadius {
+		return 1 / (pos - maxBorderPos)
+	}
+	return 0
+}
+
+func (b *Boid) moveOne() {
+	acceleration := b.calcAcceleration()
+	rWlock.Lock()
+	b.velocity = b.velocity.Add(acceleration).limit(-1, 1)
+	boidMap[int(b.position.x)][int(b.position.y)] = -1
+	b.position = b.position.Add(b.velocity)
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	rWlock.Unlock()
+}
+
+func (b *Boid) start() {
+	for {
+		b.moveOne()
+		time.Sleep(5 * time.Millisecond)
+	}
+}
+
+func createBoid(bid int) {
+	b := Boid{
+		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
+		velocity: Vector2D{(rand.Float64() * 2) - 1.0, (rand.Float64() * 2) - 1.0},
+		id:       bid,
+	}
+	boids[bid] = &b
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	go b.start()
 }
